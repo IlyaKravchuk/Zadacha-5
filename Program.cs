e8555163434a2e4f7a94fc65092acb912ac24747
IlyaKravchuk <kravilia@rambler.ru>
2014-07-15 19:25:19 +0600

    initial version

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Single edge, basis, var;
+            
+            Console.WriteLine("Введите длину стороны основания пирамиды");
+            basis = Single.Parse(Console.ReadLine());
+            Console.WriteLine("Введите длину грани пирамиды");
+            edge = Single.Parse(Console.ReadLine());  
+
+            if (((basis + edge) <= edge) || ((edge + edge) <= basis))
+                Console.WriteLine("Данная фигура не является пирамидой");
+            else
+            {
+                Console.WriteLine("Введите расстояние от основания пирамиды, на котором находится\nпараллельная плоскость");
+                var = Single.Parse(Console.ReadLine());
+
+                Pyramid tr = new Pyramid(basis, edge,var);
+
+                Console.WriteLine("Высота пирамиды = {0}", tr.height);
+                Console.WriteLine("Объем пирамиды = {0}", tr.Volume);
+                Console.WriteLine("Объем параллелипипеда, в который вписана пирамида = {0}", tr.Parallelepiped);
+                Console.WriteLine("Объем пирамиды, полученной отсечением плоскостью, параллельной\nоснованию, на заданном расстоянии от основания = {0}", tr.Pyramid2);                                
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Pyramid.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Pyramid
+    {
+        protected Single _edge, _basis, _var;
+        protected Double _height, _volume, _Par_Vol, _Volume2;
+        public Pyramid(Single basis, Single edge, Single var)
+        {
+            _edge = edge;
+            _basis = basis;
+            _var = var;
+            _height = Math.Sqrt(Math.Pow(edge, 2) - (1 / 3) * Math.Pow(basis, 2));
+            _volume = (Math.Sqrt(3) / 12) * _height * Math.Pow(basis, 2);
+            _Par_Vol = 6 * _volume;
+            _Volume2 = (Math.Sqrt(3) / 12) * (_height - _var) * Math.Pow((_height - _var) * _basis / _height, 2);
+        }
+    
+        public double Parallelepiped
+        {
+            get { return _Par_Vol; }
+        }
+        public double Pyramid2
+        {
+            get { return _Volume2; }
+        }
+        public double height
+        {
+            get { return _height; }
+        }
+        public double Volume
+        {
+            get { return _volume; }
+        }
+    }    
+}
